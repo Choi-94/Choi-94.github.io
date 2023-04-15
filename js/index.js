61a0343ec76eb17330ec97837146e313022ae847
Choi-94 <qaz6168@naver.com>
2023-04-15 18:02:41 +0900

    20230415 6시 포트폴리오

--- js/index.js
+++ js/index.js
@@ -1,10 +1,5 @@
-const fun1 = () =>{
-    alert("호출됐어요~");
-}
-//텍스트 입력시 top숫자 바꿔줘야함 !!
 portfolio = () => {
-    const portfoliobtn = document.getElementById("portfolio")
-    window.scrollTo({ top:200, behavior:"smooth"});
+    window.scrollTo({ top:800, behavior:"smooth"});
 }
 resume = () => {
     window.scrollTo({ top:1000, behavior:"smooth"});
