662acbf6dcd64bfa78d536570f6a9f6691531c7b
Choi-94 <qaz6168@naver.com>
2023-04-18 11:46:08 +0900

    20230418 포트폴리오 12시

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 portfolio = () => {
-    window.scrollTo({ top:500, behavior:"smooth"});
+    window.scrollTo({ top:800, behavior:"smooth"}); 
 }
 resume = () => {
-    window.scrollTo({ top:1700, behavior:"smooth"});
+    window.scrollTo({ top:2100, behavior:"smooth"});
 }
 contact = () => {
-    window.scrollTo({ top:3000, behavior:"smooth"});
+    window.scrollTo({ top:3300, behavior:"smooth"});
 }
